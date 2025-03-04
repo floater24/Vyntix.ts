a0aa1ff6760df390b4ab267e16532f4d533bd64c
Yuki Nishikawa <ni4kao24u@gmail.com>
2025-03-04 15:22:38 +0900

    commit season2

--- my-next-app/pages/api/auth/[...nextauth].ts
+++ my-next-app/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,4 @@
 import NextAuth from "next-auth";
 import { getAuthOptions } from "../../../src/auth/config";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("Auth request received:", req.url);
-  return NextAuth(req, res, getAuthOptions());
-}
+export default NextAuth(getAuthOptions());
